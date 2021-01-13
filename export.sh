f5bbf25ec9a8a5aee4d5d69d03107abfc20c1d94
Khalakuzzaman Apon <61594531+Apon77@users.noreply.github.com>
2021-01-14 00:18:56 +0600

    Update export.sh

--- export.sh
+++ export.sh
@@ -1,20 +1,16 @@
-#!/usr/bin/env bash
-
 # Repo URLs
-export android_manifest_url="git://github.com/AospExtended/manifest.git"
-export manifest_branch="11.x"
+rom_manifest_url="git://github.com/AospExtended/manifest.git"
+rom_manifest_branch="11.x"
 
-# Local manifest (dt,vt,kt,dependencies)
-export local_manifest_url="https://github.com/Apon77/android_.repo_local_manifests"
-export local_manifest_branch="aex"
+# Local manifest (dt,vt,kt,dependencies will sync from here)
+local_manifest_url="https://github.com/Apon77/android_.repo_local_manifests"
+local_manifest_branch="aex"
 
 # Lunch device
-export device_prefix="aosp"
-export device_name="mido"
-export build_variant="user"
+lunch="brunch aosp_mido-user"
 
-# Make Rom
-export make_rom="make aex"
+# Build command
+make_rom="make aex"
 
 # Telegram Bot
-export BOT_CHAT_ID="571213272"
+your_telegram_id="571213272"
